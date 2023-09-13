c323dee747fab17fa9b71251df313b97b65c9b14
khalid-oukha <oukhakhalid@gmail.com>
2023-09-14 01:53:50 +0200

    challenge 3

---
+++ langage c/3challenge/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //Challenge 3 : Calcule et affichage résultat en format décimal
+    int a,b;
+        printf("entre le nombre a :");
+    scanf("%d",&a);
+        printf("entre le nombre b :");
+    scanf("%d",&b);
+        printf(" %d + %d = %d :",a,b,a+b);
+        printf(" %d - %d = %d :",a,b,a-b);
+        printf(" %d * %d = %d :",a,b,a*b);
+        printf(" %d / %d = %d :",a,b,a/b);
+        printf(" %d %% %d = %d :",a,b,a%b);
+
+}
