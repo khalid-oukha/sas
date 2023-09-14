d70e3159bb2f3780854a2f7587a3b0c386d096a7
khalid oukha <oukhakhalid@gmail.com>
2023-09-14 13:59:56 +0100

    challange 4 changes

--- Les Conditions/challange5/main.c
+++ Les Conditions/challange5/main.c
@@ -7,15 +7,35 @@ int main()
     int anne,choix;
     printf("entrez un anne :");
     scanf("%d",&anne);
+    printf("------------------------ menu------------------------ \n:");
+    printf("1 -- convert to months \n:");
+    printf("2 -- convert to days \n:");
+    printf("3 -- convert to hours \n:");
+    printf("4 -- convert to minutes \n:");
+    printf("5 -- convert to seconds \n\n:");
+    printf("---------------- choose from the menu----------------------- \n:");
+    scanf("%d",&choix);
 
-    printf("1 -- months \n:");
-    printf("1 -- days \n:");
-    printf("1 -- hours \n:");
-    printf("1 -- minutes \n:");
-    printf("1 -- months \n:");
     switch (choix)
     {
-
+case 1 :
+    printf("%d year = %d month",&anne,anne*12);
+    break;
+    case 2 :
+    printf("%d year = %d days",&anne,anne*12*30);
+    break;
+    case 3 :
+    printf("%d year = %d hours",&anne,anne*12*30*24);
+    break;
+    case 4 :
+    printf("%d year = %d minute",&anne,anne*12*30*24*60);
+    break;
+    case 5 :
+    printf("%d year = %d second",&anne,anne*12*30*24*60*60);
+    break;
+    default :
+        printf("error");
+        break;
     }
     return 0;
 }
