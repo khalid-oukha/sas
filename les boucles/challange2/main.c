883070c43043d0ab0b8edf78df8402da5c7fb1bc
khalid oukha <oukhakhalid@gmail.com>
2023-09-15 17:08:00 +0100

    challange 2

---
+++ les boucles/challange2/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+
+    printf("entrez numbre des etoile :");
+    scanf("%d",&num);
+
+    for(int i=1;i<=num;i++)
+    {
+        for(int j=num;j>i;j--){
+              printf("-");
+        }
+
+
+        for(int p=1;p<i*2;p++)
+            {
+            printf("*");
+        }
+     printf("\n");
+    }
+    return 0;
+}
